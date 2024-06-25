a487613b97f0bd2a791f325f0778169155cbc3a9
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-06-26 01:55:22 +0700

    Add GSI Keys Support

--- twrp_P661N.mk
+++ twrp_P661N.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
@@ -23,11 +26,7 @@ PRODUCT_BRAND := Itel
 PRODUCT_MODEL := P661N
 PRODUCT_MANUFACTURER := ITEL
 
-PRODUCT_RELEASE_NAME := P661N
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="P661N-user 13 TP1A.220624.014 release-keys"
  
 BUILD_FINGERPRINT := Itel/P661N-GL/itel-P661N:13/TP1A.220624.014/240322V53:user/release-keys
-
-PRODUCT_GMS_CLIENTID_BASE := android-transsion
